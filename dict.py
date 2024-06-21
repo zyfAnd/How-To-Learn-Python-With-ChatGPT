4721511db46865e7876e1066fb150c4bb08dad09
yanfu <18842647603@163.com>
2024-06-21 11:12:41 +0800

    dictionary --> dict 类似其他的语言的map

---
+++ dict.py
@@ -0,0 +1,11 @@
+#类似于Java 的 map 
+# dictionary -dict key , value
+
+#list 
+names = ['michel','yanfu','xiaoqiang']
+scores  = [88,99,80]
+
+students ={'michel': 88, 'yanfu':99, 'xiaoqiang':80}
+
+student  = input()
+print(students[student])
